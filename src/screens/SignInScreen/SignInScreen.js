fc911c7c9863ba42870d55f0fa16fffc6d27d201
jihan212 <jihanbjashim@gmail.com>
2025-02-24 00:11:55 +0600

    Sign in page done

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -4,10 +4,12 @@ import {
 	Image,
 	StyleSheet,
 	useWindowDimensions,
+	ScrollView,
 } from 'react-native';
 import React, { useState } from 'react';
 import Logo from '../../../assets/images/logo.png';
-import CustomInput from '../../components/CustomInput';
+import CustomInput from '../../components/CustomInput/CustomInput';
+import CustomButton from '../../components/CustomButton/CustomButton';
 
 const SignInScreen = () => {
 	const [username, setUsername] = useState('');
@@ -15,25 +17,81 @@ const SignInScreen = () => {
 
 	const { height } = useWindowDimensions();
 
+	const onSignInPressed = () => {
+		console.warn('Sign In');
+	};
+
+	const OnForgotPasswordPressed = () => {
+		console.warn('Forgot Password');
+	};
+
+	const OnSignInFacebook = () => {
+		console.warn('Sign In With Facebook');
+	};
+	const OnSignInGoogle = () => {
+		console.warn('Sign In With Google');
+	};
+	const OnSignInApple = () => {
+		console.warn('Sign In With Apple');
+	};
+
+	const OnSignUpPress = () => {
+		console.warn('Sign Up');
+	};
+
 	return (
-		<View style={styles.root}>
-			<Image
-				source={Logo}
-				style={[styles.logo, { height: height * 0.3 }]}
-				resizeMode='contain'
-			/>
-			<CustomInput
-				placeholder='Username'
-				value={username}
-				setValue={setUsername}
-			/>
-			<CustomInput
-				placeholder='Password'
-				value={password}
-				setValue={setPassword}
-				secureTextEntry
-			/>
-		</View>
+		<ScrollView>
+			<View style={styles.root}>
+				<Image
+					source={Logo}
+					style={[styles.logo, { height: height * 0.3 }]}
+					resizeMode='contain'
+				/>
+				<CustomInput
+					placeholder='Username'
+					value={username}
+					setValue={setUsername}
+				/>
+				<CustomInput
+					placeholder='Password'
+					value={password}
+					setValue={setPassword}
+					secureTextEntry
+				/>
+				<CustomButton
+					text='Sign In'
+					onPress={onSignInPressed}
+				/>
+				<CustomButton
+					text='Forget Password ?'
+					onPress={OnForgotPasswordPressed}
+					type='TERTIARY'
+				/>
+				<CustomButton
+					text='Sign In With Facebook'
+					onPress={OnSignInFacebook}
+					bgColor='#E7EAF4'
+					fgColor='#3B5998'
+				/>
+				<CustomButton
+					text='Sign In With Google'
+					onPress={OnSignInGoogle}
+					bgColor='#FAE9EA'
+					fgColor='#DD4D44'
+				/>
+				<CustomButton
+					text='Sign In With Apple'
+					onPress={OnSignInApple}
+					bgColor='#e3e3e3'
+					fgColor='#363636'
+				/>
+				<CustomButton
+					text='Don’t have an account? Sign Up'
+					onPress={OnSignUpPress}
+					type='TERTIARY'
+				/>
+			</View>
+		</ScrollView>
 	);
 };
 
