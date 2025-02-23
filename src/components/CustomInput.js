d6b8aafe9ad099373156d2af1072c025dc2e553a
jihan212 <jihanbjashim@gmail.com>
2025-02-23 22:27:29 +0600

    first commit

---
+++ src/components/CustomInput.js
@@ -0,0 +1,29 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import React from 'react';
+
+const CustomInput = ({ value, setValue, placeholder, secureTextEntry }) => {
+	return (
+		<View style={styles.container}>
+			<TextInput
+				value={value}
+				onChangeText={setValue}
+				placeholder={placeholder}
+				styles={styles.input}
+				secureTextEntry={secureTextEntry}
+			/>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'White',
+		width: '100%',
+		borderColor: '#e8e8e8',
+		borderWidth: 1,
+		borderRadius: 5,
+	},
+	input: {},
+});
+
+export default CustomInput;
